61c8f78df6308b741ee96814e0948937e1069bfa
anubhav <itsanubhav120@gmail.com>
2025-09-04 12:53:21 +0530

    Updated Navbar to show real time & date

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,10 +2,19 @@ import * as React from 'react';
 import "./navbar.css";
 import { IoIosNotifications } from "react-icons/io";
 import { IoIosSearch } from "react-icons/io";
+
 function Navbar() {
+    const today = new Date();
+
+    const dayName = today.toLocaleDateString("en-US", { weekday: "long" });
+
+    const formattedDate = today.toLocaleDateString("en-GB"); 
+
     return (
         <div className='nav-main-container'>
-            <div><p className='nav-main-text'>Dash<span>Board</span></p></div>
+            <div>
+                <p className='nav-main-text'>Dash<span>Board</span></p>
+            </div>
             <div className='nav-search-container'>
                 <input placeholder='Search your task here...' />
                 <div className='task-read'>
@@ -14,14 +23,16 @@ function Navbar() {
             </div>
             <div className='nav-notification-container'>
                 <div className='task-read'>
-                    <IoIosNotifications  className='read-icon' />
+                    <IoIosNotifications className='read-icon' />
                 </div>
                 <div>
-                    <p className='nav-day-text'>Tuesday</p>
-                    <p className='nav-date-text'>20/06/2023</p>
+                    
+                    <p className='nav-day-text'>{dayName}</p>
+                    <p className='nav-date-text'>{formattedDate}</p>
                 </div>
             </div>
         </div>
     )
 }
-export default Navbar
+
+export default Navbar;
