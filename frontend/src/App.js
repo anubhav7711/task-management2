a3fc5ad0f307a59ac5137b566c317e77f04a9186
Anubhav BansaL <itsanubhav120@gmailc.com>
2025-10-30 19:14:44 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import Register from './layouts/auth/Register.jsx';
-// import Login from './layouts/auth/Login.jsx';
-// import Dashboard from './layouts/dashboard/Dashboard.jsx';
-// import Employees from './layouts/employees/Employees.jsx'
-// import Projects from './layouts/projects/Projects.jsx'
-// import Tasks from './layouts/tasks/Tasks.jsx'
-// import Timesheets from './layouts/timesheets/Timesheets.jsx'
-// import Attendance from './layouts/attendance/Attendance.jsx'
-// import ProtectedRoute from './components/ProtectedRoute.jsx';
-// function App() {
-//   return (
-//     <>
-
-//       <Router>
-//         <Routes>
-//           <Route path='/' element={<Login />} />
-//           <Route path='/register' element={<Register />} />
-//           <Route path='/admin/dashboard' element={<Dashboard />} />
-//           <Route path='/admin/employees' element={<Employees />} />
-//           <Route path='/admin/projects' element={<Projects />} />
-//           <Route path='/admin/tasks' element={<Tasks />} />
-//           <Route path='/admin/timesheets' element={<Timesheets />} />
-//           <Route path='/admin/attendance' element={<Attendance />} />
-//            <Route path='/admin/dashboard'element={<ProtectedRoute><Dashboard /></ProtectedRoute>}/>
-//         </Routes>
-//       </Router>
-//     </>
-//   )
-// }
-
-// export default App;
-
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './layouts/auth/Register.jsx';
 import Login from './layouts/auth/Login.jsx';
